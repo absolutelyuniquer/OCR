62b5b4ac0bf2f196ee2f81fd99552115bd8a40f6
absolutelyuniquer <1156326866@qq.com>
2024-05-19 16:51:29 +0800

    2024-05-19 16:51:29

--- main.py
+++ main.py
@@ -135,3 +135,4 @@ def transcribe(
         Extract word-level timestamps using the cross-attention pattern and dynamic time warping,
         and include the timestamps for each word in each segment.
 
+    prepend_punctuations: str
