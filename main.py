3f31057c07f99400b6f782f89d3d4d38d4994b43
absolutelyuniquer <1156326866@qq.com>
2024-11-09 01:31:17 +0800

    2024-11-09 01:31:17

--- main.py
+++ main.py
@@ -281,3 +281,4 @@ def transcribe(
 
     condition_on_previous_text: bool
         if True, the previous output of the model is provided as a prompt for the next window;
+        disabling may make the text inconsistent across windows, but the model becomes less prone to
