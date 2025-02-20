7676c7b604a08b8c1803bf691d21a51fe3d896b3
absolutelyuniquer <1156326866@qq.com>
2025-02-20 22:17:37 +0800

    2025-02-20 22:17:37

--- main.py
+++ main.py
@@ -346,3 +346,4 @@ def transcribe(
     initial_prompt: Optional[str] = None,
     word_timestamps: bool = False,
     prepend_punctuations: str = "\"'“¿([{-",
+    append_punctuations: str = "\"'.。,，!！?？:：”)]}、",
