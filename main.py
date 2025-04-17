cdf239fa759ddeeb735f3b78bc2d3799b28b4efc
absolutelyuniquer <1156326866@qq.com>
2025-04-18 01:43:10 +0800

    2025-04-18 01:43:10

--- main.py
+++ main.py
@@ -388,3 +388,4 @@ def transcribe(
         and include the timestamps for each word in each segment.
 
     prepend_punctuations: str
+        If word_timestamps is True, merge these punctuation symbols with the next word
