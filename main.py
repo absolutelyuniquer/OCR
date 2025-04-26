0e18a60fba3a1cb1c83a889b4f362f7c5bc3e508
absolutelyuniquer <1156326866@qq.com>
2025-04-26 15:29:09 +0800

    2025-04-26 15:29:08

--- main.py
+++ main.py
@@ -397,3 +397,4 @@ def transcribe(
         Optional text to provide as a prompt for the first window. This can be used to provide, or
         "prompt-engineer" a context for transcription, e.g. custom vocabularies or proper nouns
         to make it more likely to predict those word correctly.
+
