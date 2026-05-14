fb4ffd18e5b9c68096d0c82e545791c3b062ae1c
absolutelyuniquer <1156326866@qq.com>
2026-05-14 22:47:27 +0800

    2026-05-14 22:47:27

--- main.py
+++ main.py
@@ -478,3 +478,4 @@ def transcribe(
         over sampled tokens is below `logprob_threshold`, consider the segment as silent
 
     condition_on_previous_text: bool
+        if True, the previous output of the model is provided as a prompt for the next window;
